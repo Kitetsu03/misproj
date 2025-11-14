3e9a3470d6772c3c47299957138347947ca0196f
Marc Narvel <marcnarvelortegoza@gmail.com>
2025-11-14 19:44:53 +0800

    Update

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -1,3 +1,5 @@
+import { Chart as ChartJS } from "chart.js/auto";
+import { Bar, Doughnut, Line } from "react-chartjs-2";
 import AdminNav from "../../components/AdminNav";
 
 function AdminDashboard() {
@@ -48,19 +50,55 @@ function AdminDashboard() {
               {/* chart1 */}
             </div>
             <div className="card p-5 col-span-2 row-span-2 rounded-2xl">
-              <div className="text-white ">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
-                quo labore et excepturi eos, dolores veniam est? Ducimus ut
-                molestiae sapiente nesciunt facilis similique voluptatem,
-                mollitia expedita? Ex, explicabo ut.
+              <div className="">
+                <Bar
+                  data={{
+                    labels: [
+                      "Jan",
+                      "Feb",
+                      "Mar",
+                      "Apr",
+                      "May",
+                      "Jun",
+                      "Jul",
+                      "Aug",
+                      "Sept",
+                      "Oct",
+                      "Nov",
+                      "Dec",
+                    ],
+                    datasets: [
+                      {
+                        label: "My First Dataset",
+                        data: [65, 59, 80, 81, 56, 55, 40, 65, 59, 80, 81, 56],
+                        fill: false,
+                        borderColor: "rgb(75, 192, 192)",
+                        tension: 0.1,
+                      },
+                    ],
+                  }}
+                />
               </div>
             </div>
             <div className="card p-5 col-span-2 row-span-2 rounded-2xl">
-              <div className="text-black">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
-                quo labore et excepturi eos, dolores veniam est? Ducimus ut
-                molestiae sapiente nesciunt facilis similique voluptatem,
-                mollitia expedita? Ex, explicabo ut.
+              <div className="">
+                <Doughnut
+                  data={{
+                    labels: ["Red", "Blue", "Yellow"],
+                    datasets: [
+                      {
+                        label: "My First Dataset",
+                        data: [300, 50, 100],
+                        backgroundColor: [
+                          "rgb(255, 99, 132)",
+                          "rgb(54, 162, 235)",
+                          "rgb(255, 205, 86)",
+                        ],
+                        hoverOffset: 4,
+                      },
+                    ],
+                  }}
+                />
               </div>
             </div>
             <div className="card p-5 row-span-2 col-start-1 row-start-4 rounded-2xl">
