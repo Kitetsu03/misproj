860984cfe80f611944d820270d44bc30446886ec
Marc Narvel <marcnarvelortegoza@gmail.com>
2025-12-17 15:59:41 +0800

    commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,7 @@
 import { useState, useEffect } from "react";
 
-import SidebarItem from "./sidebarItem";
-import isLogo from "/IS-LOGO.svg";
-import { ChevronFirst } from "lucide-react";
+import AdminNav from "./AdminNav";
+import { FaBars } from "react-icons/fa";
 
 const Sidebar = ({ activeComponent, setActiveComponent }) => {
   const [isOpen, setIsOpen] = useState(() => {
@@ -42,11 +41,8 @@ const Sidebar = ({ activeComponent, setActiveComponent }) => {
             }`}
           >
             {isOpen && (
-              // <h1 className="overflow-hidden text-sm">
-              //   Inventory Mangement System
-              // </h1>
               <span className="flex items-center gap-2 text-accent">
-                <img src={isLogo} alt="IS-LOGO" className="w-8" />
+                <div className="my-logo"></div>
                 <span className="hidden md:inline">Inventory System</span>
                 <span className="md:hidden">IS</span>
               </span>
@@ -55,7 +51,7 @@ const Sidebar = ({ activeComponent, setActiveComponent }) => {
               className="p-1.5 rounded-lg bg-secondary hover:bg-active transition-all duration-200"
               onClick={handleClick}
             >
-              <ChevronFirst
+              <FaBars
                 className={`transition-transform duration-300 ${
                   isOpen ? "rotate-0" : "rotate-180"
                 }`}
@@ -63,7 +59,7 @@ const Sidebar = ({ activeComponent, setActiveComponent }) => {
             </button>
           </div>
           <hr className="border-b border-active" />
-          <SidebarItem
+          <AdminNav
             isOpen={isOpen}
             activeComponent={activeComponent}
             setActiveComponent={setActiveComponent}
--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -1,4 +1,4 @@
-import AdminNav from "../../components/AdminNav.jsx";
+import SideBar from "../../components/Sidebar.jsx";
 import Card from "../../components/Card.jsx";
 import QuickActions from "../../components/QuickActions.jsx";
 import RecentActivities from "../../components/RecentActivities.jsx";
@@ -70,47 +70,45 @@ function AdminDashboard() {
   ];
   return (
     <>
-      <div className="min-h-dvh grid grid-cols-[auto_1fr]">
-        <AdminNav />
-        <div className="body">
-          <header className="p-5 font-secondary">
-            <h1 className="text-2xl md:text-5xl text-white font-extrabold">
-              CHURCH MANAGEMENT DASHBOARD
-            </h1>
-            <p className="pt-1 text-white">
-              Welcome back! Here's an overview of your church's key metrics and
-              recent activity.
-            </p>
-          </header>
-          <div className="flex flex-col gap-2 p-2 md:grid md:grid-cols-4 md:grid-rows-1 md:gap-3 md:p-3 font-secondary">
-            {infoCard.map((info) => (
-              <Card
-                key={info.id}
-                title={info.title}
-                icon={info.icon}
-                info={info.info}
-                value={info.value}
-                desc={info.desc}
+      <SideBar />
+      <div className="body">
+        <header className="p-5 font-secondary">
+          <h1 className="text-2xl md:text-5xl text-white font-extrabold">
+            CHURCH MANAGEMENT DASHBOARD
+          </h1>
+          <p className="pt-1 text-white">
+            Welcome back! Here's an overview of your church's key metrics and
+            recent activity.
+          </p>
+        </header>
+        <div className="flex flex-col gap-2 p-2 md:grid md:grid-cols-4 md:grid-rows-1 md:gap-3 md:p-3 font-secondary">
+          {infoCard.map((info) => (
+            <Card
+              key={info.id}
+              title={info.title}
+              icon={info.icon}
+              info={info.info}
+              value={info.value}
+              desc={info.desc}
+            />
+          ))}
+          {/* LineChart */}
+          <LineChart />
+          {/* doughnut chart */}
+          <DoughnutChart />
+          <div className="card p-5 row-span-2 col-start-1 row-start-4 rounded-2xl font-secondary">
+            <h2 className="text-2xl font-semibold">QUICK ACTIONS</h2>
+            <p className="pb-3">Common tasks</p>
+            {quickActionButtons.map((buttons) => (
+              <QuickActions
+                key={buttons.id}
+                icon={buttons.icon}
+                title={buttons.title}
               />
             ))}
-            {/* LineChart */}
-            <LineChart />
-            {/* doughnut chart */}
-            <DoughnutChart />
-            <div className="card p-5 row-span-2 col-start-1 row-start-4 rounded-2xl font-secondary">
-              <h2 className="text-2xl font-semibold">QUICK ACTIONS</h2>
-              <p className="pb-3">Common tasks</p>
-              {quickActionButtons.map((buttons) => (
-                <QuickActions
-                  key={buttons.id}
-                  icon={buttons.icon}
-                  title={buttons.title}
-                />
-              ))}
-            </div>
-            <div className="card p-5 col-span-3 row-span-2 col-start-2 row-start-4 rounded-2xl">
-              <RecentActivities />
-            </div>
+          </div>
+          <div className="card p-5 col-span-3 row-span-2 col-start-2 row-start-4 rounded-2xl">
+            <RecentActivities />
           </div>
         </div>
       </div>
