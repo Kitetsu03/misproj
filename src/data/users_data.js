4be232a43d900d2d76cbb209d8ae909383d3a155
Marc Narvel <marcnarvelortegoza@gmail.com>
2026-01-30 16:23:02 +0800

    update

---
+++ src/data/users_data.js
@@ -0,0 +1,12 @@
+export const data = [
+  {
+    username: "admin@gmail.com",
+    password: "P@ssw0rd",
+    fullName: "Administration",
+  },
+  {
+    username: "user@gmail.com",
+    password: "P@ssw0rd",
+    fullName: "Marc Narvel Ortegoza",
+  },
+];
