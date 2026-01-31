431e714c918aabdfb57db0ed44ff8137f76bb3e0
Jc Miranda <jc.miranda7070@gmail.com>
2026-01-30 23:39:01 -0800

    os

--- src/components/FinanceTab.jsx
+++ src/components/FinanceTab.jsx
@@ -24,6 +24,7 @@ export const FinanceTab = () => {
         mission: "10",
         pledge: "100",
         timedate: "2025-01-15",
+        refno: "001",
       },
       {
         id: 2,
@@ -34,6 +35,7 @@ export const FinanceTab = () => {
         mission: "5",
         pledge: "75",
         timedate: "2025-01-14",
+        refno: "002",
       },
       {
         id: 3,
@@ -44,6 +46,7 @@ export const FinanceTab = () => {
         mission: "8",
         pledge: "60",
         timedate: "2025-01-13",
+        refno: "003",
       },
       {
         id: 4,
@@ -54,10 +57,12 @@ export const FinanceTab = () => {
         mission: "15",
         pledge: "80",
         timedate: "2025-01-12",
+        refno: "004",
       },
     ],
     []
   );
+  
 
   const filteredUsers = useMemo(() => {
     if (!query) return users;
@@ -300,7 +305,7 @@ export const FinanceTab = () => {
               <tbody className="space-y-4">
                 {filteredUsers.map((u) => (
                   <tr key={u.id} className="border-b border-black/20 text-sm">
-                    <td className="py-2">{u.name}</td>
+                    <td className="py-2">{u.refno}</td>
                     <td>{u.network}</td>
 
                     {/* Roles */}
