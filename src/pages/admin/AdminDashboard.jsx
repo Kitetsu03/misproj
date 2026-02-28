8b24b19460ac755d282727811f9a2c2d094b9a06
kitetsu <marcnarvelortegoza@gmail.com>
2026-02-28 15:38:07 +0800

    sdasd

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -139,31 +139,19 @@ function AdminDashboard() {
                 <WhiteButton
                   val="Generate attendance Report"
                   exc="Add Member"
-                  icon={
-                    <>
-                      <HiOutlinePencilAlt />
-                    </>
-                  }
+                  icon={<HiOutlinePencilAlt />}
                   comp={<AddNewMember />}
                 />
                 <WhiteButton
                   val="Generate financial report"
                   exc="Add Member"
-                  icon={
-                    <>
-                      <IoMdAdd />
-                    </>
-                  }
+                  icon={<IoMdAdd />}
                   comp={<AddNewMember />}
                 />
                 <WhiteButton
                   val="Send Announcement"
                   exc="Add Member"
-                  icon={
-                    <>
-                      <RiSendPlaneFill />
-                    </>
-                  }
+                  icon={<RiSendPlaneFill />}
                   comp={<AddNewMember />}
                 />
               </div>
